ea64c0a1cfbe722e9d5e32e186668932406c5d1c
mtarikg <mtarikgol@gmail.com>
2021-06-10 20:43:13 +0300

    mainPage edited & addQuestion completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:question_world/Core/categories.dart';
+import 'package:question_world/Core/mainPage.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Categories("Categories"),
+      home: MainPage(),
     );
   }
 }
